d415fd7cf23dd0db7c62d81fda143a5ba36c824f
fsousa1987 <franciscoeds1987@gmail.com>
2021-09-28 19:36:24 -0300

    3.7 -Conhecendo as estratégias para geração de identificador com GeneratedValue

---
+++ src/test/java/com/francisco/ecommerce/mapeamentobasico/EstrategiaChavePrimariaTest.java
@@ -0,0 +1,24 @@
+package com.francisco.ecommerce.mapeamentobasico;
+
+import com.francisco.ecommerce.EntityManagerTest;
+import com.francisco.ecommerce.model.Categoria;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EstrategiaChavePrimariaTest extends EntityManagerTest {
+
+  @Test
+  public void testarEstrategiaAuto() {
+    Categoria categoria = new Categoria();
+    categoria.setNome("Eletrônicos");
+
+    entityManager.getTransaction().begin();
+    entityManager.persist(categoria);
+    entityManager.getTransaction().commit();
+
+    entityManager.clear();
+
+    Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+    Assertions.assertNotNull(categoriaVerificacao);
+  }
+}
