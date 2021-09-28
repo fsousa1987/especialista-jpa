f3805b0b340bb227084de232a1eae66f75cbd2b8
fsousa1987 <franciscoeds1987@gmail.com>
2021-09-28 10:07:36 -0300

    2.11 - Atualizando objetos com o método merge

--- src/test/java/com/francisco/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/francisco/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -9,6 +9,26 @@ import org.junit.jupiter.api.Test;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
   @Test
+  public void atualizarObjeto() {
+    Produto produto = new Produto();
+
+    produto.setId(1);
+    produto.setNome("Kindle Paperwhite");
+    produto.setDescricao("Conheça o novo Kindle.");
+    produto.setPreco(new BigDecimal(599));
+
+    entityManager.getTransaction().begin();
+    entityManager.merge(produto);
+    entityManager.getTransaction().commit();
+
+    entityManager.clear();
+
+    Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+    Assertions.assertNotNull(produtoVerificacao);
+    Assertions.assertEquals("Kindle Paperwhite", produtoVerificacao.getNome());
+  }
+
+  @Test
   public void removerObjeto() {
     Produto produto = entityManager.find(Produto.class, 3);
 
