b2377d6f1135402f67f30fe603542820301d32f4
fsousa1987 <franciscoeds1987@gmail.com>
2021-09-28 08:17:05 -0300

    2.4 - Criando o EntityManager

---
+++ src/main/java/com/francisco/ecommerce/util/IniciarUnidadeDePersistencia.java
@@ -0,0 +1,23 @@
+package com.francisco.ecommerce.util;
+
+import com.francisco.ecommerce.model.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class IniciarUnidadeDePersistencia {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory("Ecommerce-PU");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        System.out.println(produto.getNome());
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
