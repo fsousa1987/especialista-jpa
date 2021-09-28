b2f1d5e155bd0304d2c372cf4dec6bf84d33e66c
fsousa1987 <franciscoeds1987@gmail.com>
2021-09-28 11:07:27 -0300

    2.16 - Desanexando objetos do contexto de persistência com o método detach

--- src/test/java/com/francisco/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/francisco/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -9,6 +9,21 @@ import org.junit.jupiter.api.Test;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
   @Test
+  public void impedirOperacaoComBancoDeDados() {
+    Produto produto = entityManager.find(Produto.class, 1);
+    entityManager.detach(produto);
+
+    entityManager.getTransaction().begin();
+    produto.setNome("Kindle Paperwhite 2ª Geração");
+    entityManager.getTransaction().commit();
+
+    entityManager.clear();
+
+    Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+    Assertions.assertEquals("Kindle", produtoVerificacao.getNome());
+  }
+
+  @Test
   public void mostrarDiferencaPersistMerge() {
     Produto produtoPersist = new Produto();
 
