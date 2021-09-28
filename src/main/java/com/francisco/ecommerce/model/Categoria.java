c5e4b077ea68239ebed301a1664857285c4c7d62
fsousa1987 <franciscoeds1987@gmail.com>
2021-09-28 19:48:46 -0300

    3.8 - Configurando a geração de identificador com SequenceGenerator

--- src/main/java/com/francisco/ecommerce/model/Categoria.java
+++ src/main/java/com/francisco/ecommerce/model/Categoria.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,7 +17,8 @@ import lombok.Setter;
 public class Categoria {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+  @SequenceGenerator(name = "seq", sequenceName = "sequencia_chave_primaria")
   private Integer id;
 
   private String nome;
