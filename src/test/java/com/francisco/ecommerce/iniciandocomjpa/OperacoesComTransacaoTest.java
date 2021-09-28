5666929c1b745e6a3db6bd9624524fff59c42325
fsousa1987 <franciscoeds1987@gmail.com>
2021-09-28 09:44:45 -0300

    2.9 - Inserindo o primeiro objeto com o método persist

--- src/test/java/com/francisco/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/francisco/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -1,9 +1,31 @@
 package com.francisco.ecommerce.iniciandocomjpa;
 
 import com.francisco.ecommerce.EntityManagerTest;
+import com.francisco.ecommerce.model.Produto;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class OperacoesComTransacaoTest extends EntityManagerTest {
+
+  @Test
+  public void inserirOPrimeiroObjeto() {
+    Produto produto = new Produto();
+
+    produto.setId(2);
+    produto.setNome("Câmera Canon");
+    produto.setDescricao("A melhor definição para suas fotos.");
+    produto.setPreco(new BigDecimal(5000));
+
+    entityManager.getTransaction().begin();
+    entityManager.persist(produto);
+    entityManager.getTransaction().commit();
+
+    entityManager.clear();
+
+    Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+    Assertions.assertNotNull(produtoVerificacao);
+  }
 
   @SuppressWarnings("CommentedOutCode")
   @Test
