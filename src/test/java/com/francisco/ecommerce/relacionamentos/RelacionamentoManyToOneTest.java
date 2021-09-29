b10c358c3715637c94cd0bc3f94f35d28323d497
fsousa1987 <franciscoeds1987@gmail.com>
2021-09-29 10:00:03 -0300

    4.3 - Mapeando relacionamentos muitos-para-um

--- src/test/java/com/francisco/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
+++ src/test/java/com/francisco/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -2,7 +2,9 @@ package com.francisco.ecommerce.relacionamentos;
 
 import com.francisco.ecommerce.EntityManagerTest;
 import com.francisco.ecommerce.model.Cliente;
+import com.francisco.ecommerce.model.ItemPedido;
 import com.francisco.ecommerce.model.Pedido;
+import com.francisco.ecommerce.model.Produto;
 import com.francisco.ecommerce.model.StatusPedido;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -30,5 +32,34 @@ public class RelacionamentoManyToOneTest extends EntityManagerTest {
 
     Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
     Assertions.assertNotNull(pedidoVerificacao.getCliente());
+  }
+
+  @Test
+  public void verificarRelacionamentoItemPedido() {
+    Cliente cliente = entityManager.find(Cliente.class, 1);
+    Produto produto = entityManager.find(Produto.class, 1);
+
+    Pedido pedido = new Pedido();
+    pedido.setStatus(StatusPedido.AGUARDANDO);
+    pedido.setDataPedido(LocalDateTime.now());
+    pedido.setTotal(BigDecimal.TEN);
+    pedido.setCliente(cliente);
+
+    ItemPedido itemPedido = new ItemPedido();
+    itemPedido.setPrecoProduto(produto.getPreco());
+    itemPedido.setQuantidade(1);
+    itemPedido.setPedido(pedido);
+    itemPedido.setProduto(produto);
+
+    entityManager.getTransaction().begin();
+    entityManager.persist(pedido);
+    entityManager.persist(itemPedido);
+    entityManager.getTransaction().commit();
+
+    entityManager.clear();
+
+    ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+    Assertions.assertNotNull(itemPedidoVerificacao.getPedido());
+    Assertions.assertNotNull(itemPedidoVerificacao.getProduto());
   }
 }
