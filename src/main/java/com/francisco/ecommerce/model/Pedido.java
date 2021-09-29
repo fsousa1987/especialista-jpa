95d74fe4650ff9ff3b58f0fc91ef79369a962bb5
fsousa1987 <franciscoeds1987@gmail.com>
2021-09-29 10:14:54 -0300

    4.5 - Mapeando relacionamentos um-para-muitos

--- src/main/java/com/francisco/ecommerce/model/Pedido.java
+++ src/main/java/com/francisco/ecommerce/model/Pedido.java
@@ -2,6 +2,7 @@ package com.francisco.ecommerce.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -12,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -29,6 +31,9 @@ public class Pedido {
   @ManyToOne
   @JoinColumn(name = "cliente_id")
   private Cliente cliente;
+
+  @OneToMany(mappedBy = "pedido")
+  private List<ItemPedido> itens;
 
   @Column(name = "data_pedido")
   private LocalDateTime dataPedido;
--- src/test/java/com/francisco/ecommerce/relacionamentos/RelacionamentoOneToManyTest.java
+++ src/test/java/com/francisco/ecommerce/relacionamentos/RelacionamentoOneToManyTest.java
@@ -2,7 +2,9 @@ package com.francisco.ecommerce.relacionamentos;
 
 import com.francisco.ecommerce.EntityManagerTest;
 import com.francisco.ecommerce.model.Cliente;
+import com.francisco.ecommerce.model.ItemPedido;
 import com.francisco.ecommerce.model.Pedido;
+import com.francisco.ecommerce.model.Produto;
 import com.francisco.ecommerce.model.StatusPedido;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -30,5 +32,33 @@ public class RelacionamentoOneToManyTest extends EntityManagerTest {
 
     Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
     Assertions.assertFalse(clienteVerificacao.getPedidos().isEmpty());
+  }
+
+  @Test
+  public void verificarRelacionamentoPedido() {
+    Cliente cliente = entityManager.find(Cliente.class, 1);
+    Produto produto = entityManager.find(Produto.class, 1);
+
+    Pedido pedido = new Pedido();
+    pedido.setStatus(StatusPedido.AGUARDANDO);
+    pedido.setDataPedido(LocalDateTime.now());
+    pedido.setTotal(BigDecimal.TEN);
+    pedido.setCliente(cliente);
+
+    ItemPedido itemPedido = new ItemPedido();
+    itemPedido.setPrecoProduto(produto.getPreco());
+    itemPedido.setQuantidade(1);
+    itemPedido.setPedido(pedido);
+    itemPedido.setProduto(produto);
+
+    entityManager.getTransaction().begin();
+    entityManager.persist(pedido);
+    entityManager.persist(itemPedido);
+    entityManager.getTransaction().commit();
+
+    entityManager.clear();
+
+    Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+    Assertions.assertFalse(pedidoVerificacao.getItens().isEmpty());
   }
 }
