e457951c789da2da6103815331bd65f1a1223d2c
fsousa1987 <franciscoeds1987@gmail.com>
2021-10-01 08:40:23 -0300

    5.1 - Estados e ciclo de vida dos objetos

---
+++ src/test/java/com/francisco/ecommerce/conhecendoentitymanager/EstadosECicloDeVidaTest.java
@@ -0,0 +1,22 @@
+package com.francisco.ecommerce.conhecendoentitymanager;
+
+import com.francisco.ecommerce.EntityManagerTest;
+import com.francisco.ecommerce.model.Categoria;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("unused")
+public class EstadosECicloDeVidaTest extends EntityManagerTest {
+
+  @Test
+  public void analisarEstados() {
+    Categoria categoriaNovo = new Categoria();
+    Categoria categoriaGerenciadaMerge = entityManager.merge(categoriaNovo);
+
+    Categoria categoriaGerenciada = entityManager.find(Categoria.class, 1);
+
+    entityManager.remove(categoriaGerenciada);
+    entityManager.persist(categoriaGerenciada);
+
+    entityManager.detach(categoriaGerenciada);
+  }
+}
