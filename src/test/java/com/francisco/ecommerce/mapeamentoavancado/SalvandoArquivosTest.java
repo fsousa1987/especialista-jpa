a3d042ca16142cdf1f60106df33de9207d3a7724
fsousa1987 <franciscoeds1987@gmail.com>
2021-10-07 08:57:16 -0300

    6.12 - Mapeando e persistindo dados de arquivos com Lob

---
+++ src/test/java/com/francisco/ecommerce/mapeamentoavancado/SalvandoArquivosTest.java
@@ -0,0 +1,52 @@
+package com.francisco.ecommerce.mapeamentoavancado;
+
+import com.francisco.ecommerce.EntityManagerTest;
+import com.francisco.ecommerce.model.NotaFiscal;
+import com.francisco.ecommerce.model.Pedido;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Objects;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("CommentedOutCode")
+public class SalvandoArquivosTest extends EntityManagerTest {
+
+  @Test
+  public void salvarXmlNota() {
+    Pedido pedido = entityManager.find(Pedido.class, 1);
+
+    NotaFiscal notaFiscal = new NotaFiscal();
+    notaFiscal.setPedido(pedido);
+    notaFiscal.setDataEmissao(new Date());
+    notaFiscal.setXml(carregarNotaFiscal());
+
+    entityManager.getTransaction().begin();
+    entityManager.persist(notaFiscal);
+    entityManager.getTransaction().commit();
+
+    NotaFiscal notaFiscalVerificacao = entityManager.find(NotaFiscal.class, notaFiscal.getId());
+    Assertions.assertNotNull(notaFiscalVerificacao.getXml());
+    Assertions.assertTrue(notaFiscalVerificacao.getXml().length > 0);
+
+        /*
+        try {
+            OutputStream out = new FileOutputStream(
+                    Files.createFile(Paths.get(
+                            System.getProperty("user.home") + "/xml.xml")).toFile());
+            out.write(notaFiscalVerificacao.getXml());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+         */
+  }
+
+  private static byte[] carregarNotaFiscal() {
+    try {
+      return Objects.requireNonNull(SalvandoArquivosTest.class.getResourceAsStream(
+          "/nota-fiscal.xml")).readAllBytes();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
