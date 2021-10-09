26fac75d3edee539ea2ca4304df55207c50dd9a4
fsousa1987 <franciscoeds1987@gmail.com>
2021-10-09 08:03:00 -0300

    6.18 - Mapeando herança com estratégia de uma tabela por classe (table per class)

--- src/main/java/com/francisco/ecommerce/model/Pagamento.java
+++ src/main/java/com/francisco/ecommerce/model/Pagamento.java
@@ -1,7 +1,5 @@
 package com.francisco.ecommerce.model;
 
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -10,16 +8,15 @@ import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@DiscriminatorColumn(name = "tipo_pagamento", discriminatorType = DiscriminatorType.STRING)
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@DiscriminatorColumn(name = "tipo_pagamento", discriminatorType = DiscriminatorType.STRING)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Entity
-@Table(name = "pagamento")
+//@Table(name = "pagamento")
 public abstract class Pagamento extends EntidadeBaseInteger {
 
   @MapsId
--- src/main/java/com/francisco/ecommerce/model/PagamentoCartao.java
+++ src/main/java/com/francisco/ecommerce/model/PagamentoCartao.java
@@ -1,15 +1,16 @@
 package com.francisco.ecommerce.model;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@DiscriminatorValue("cartao")
+//@DiscriminatorValue("cartao")
 @Entity
+@Table(name = "pagamento_cartao")
 public class PagamentoCartao extends Pagamento {
 
   @Column(name = "numero_cartao")
