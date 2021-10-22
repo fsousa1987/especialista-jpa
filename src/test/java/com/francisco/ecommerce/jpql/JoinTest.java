73b588fae3beb2c6f63277538eae8326b0c43843
fsousa1987 <franciscoeds1987@gmail.com>
2021-10-22 07:28:30 -0300

    9.8 - Fazendo o join e usando o fetch

--- src/test/java/com/francisco/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/francisco/ecommerce/jpql/JoinTest.java
@@ -1,12 +1,25 @@
 package com.francisco.ecommerce.jpql;
 
 import com.francisco.ecommerce.EntityManagerTest;
+import com.francisco.ecommerce.model.Pedido;
 import java.util.List;
 import javax.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class JoinTest extends EntityManagerTest {
+
+  @Test
+  public void fazerJoinFetch() {
+    String jpql =
+        "select p from Pedido p left join fetch p.pagamento join fetch p.cliente "
+            + "left join fetch p.notaFiscal";
+
+    TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+
+    List<Pedido> lista = typedQuery.getResultList();
+    Assertions.assertFalse(lista.isEmpty());
+  }
 
   @Test
   public void fazerLeftJoin() {
