395c8dbc93a742553d10877f68581f827b83cff3
fsousa1987 <franciscoeds1987@gmail.com>
2021-10-25 06:37:56 -0300

    9.10 - Consultando pedidos com produto específico

--- src/test/java/com/francisco/ecommerce/jpql/PathExpressionTest.java
+++ src/test/java/com/francisco/ecommerce/jpql/PathExpressionTest.java
@@ -1,12 +1,26 @@
 package com.francisco.ecommerce.jpql;
 
 import com.francisco.ecommerce.EntityManagerTest;
+import com.francisco.ecommerce.model.Pedido;
 import java.util.List;
 import javax.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PathExpressionTest extends EntityManagerTest {
+
+  @SuppressWarnings("CommentedOutCode")
+  @Test
+  public void buscarPedidosComProdutoEspecifico() {
+    String jpql = "select p from Pedido p join p.itens i where i.id.produtoId = 1";
+//        String jpql = "select p from Pedido p join p.itens i where i.produto.id = 1";
+//        String jpql = "select p from Pedido p join p.itens i join i.produto pro where pro.id = 1";
+
+    TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+
+    List<Pedido> lista = typedQuery.getResultList();
+    Assertions.assertEquals(2, lista.size());
+  }
 
   @Test
   public void usarPathExpressions() {
