c0db372386362639a818d83ceed554f01f1fc6e8
fsousa1987 <franciscoeds1987@gmail.com>
2021-10-26 06:58:45 -0300

    9.13 - Usando expressões condicionais is null e is empty

--- src/test/java/com/francisco/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ src/test/java/com/francisco/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -6,7 +6,30 @@ import javax.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("CommentedOutCode")
 public class ExpressoesCondicionaisTest extends EntityManagerTest {
+
+//  @Test
+//  public void usarIsNull() {
+//
+//    String jpql = "select p from Produto p where p.foto is null";
+//
+//    TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+//
+//    List<Object[]> lista = typedQuery.getResultList();
+//
+//    Assertions.assertFalse(lista.isEmpty());
+//  }
+
+  @Test
+  public void usarIsEmpty() {
+    String jpql = "select p from Produto p where p.categorias is empty";
+
+    TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+    List<Object[]> lista = typedQuery.getResultList();
+    Assertions.assertFalse(lista.isEmpty());
+  }
 
   @Test
   public void usarExpressaoCondicionalLike() {
