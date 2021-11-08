b0f3645cd4261efe053c0d0c6f717ec302fe9f5b
fsousa1987 <franciscoeds1987@gmail.com>
2021-11-08 06:45:30 -0300

    9.30 - Usando a cláusula where com group by

--- src/test/java/com/francisco/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/francisco/ecommerce/jpql/GroupByTest.java
@@ -8,6 +8,36 @@ import org.junit.jupiter.api.Test;
 
 public class GroupByTest extends EntityManagerTest {
 
+  @Test
+  public void agruparEFiltrarResultado() {
+//         Total de vendas por mês.
+//        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
+//                " from Pedido p " +
+//                " where year(p.dataCriacao) = year(current_date) " +
+//                " group by year(p.dataCriacao), month(p.dataCriacao) ";
+
+//         Total de vendas por categoria.
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.produto pro join pro.categorias c join ip.pedido p " +
+//                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) = month(current_date) " +
+//                " group by c.id";
+
+//        Total de vendas por cliente
+
+    String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+        " join ip.pedido p join p.cliente c join ip.pedido p " +
+        " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) >= (month(current_date) - 3) " +
+        " group by c.id";
+
+    TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+    List<Object[]> lista = typedQuery.getResultList();
+
+    Assertions.assertFalse(lista.isEmpty());
+
+    lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+  }
+
   @SuppressWarnings("CommentedOutCode")
   @Test
   public void agruparResultado() {
