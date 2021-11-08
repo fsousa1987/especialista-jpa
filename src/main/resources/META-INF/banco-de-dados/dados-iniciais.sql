b163e9745d63c03c8c6d1015dad10d711d8c164f
fsousa1987 <franciscoeds1987@gmail.com>
2021-11-08 07:07:35 -0300

    9.32 - Usando a expressão case

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -2,7 +2,6 @@ insert into produto (id, nome, preco, data_criacao, descricao) values (1, 'Kindl
 insert into produto (id, nome, preco, data_criacao, descricao) values (3, 'Câmera GoPro Hero 7', 1400.0, current_date - interval 1 day, 'Desempenho 2x melhor.');
 insert into produto (id, nome, preco, data_criacao, descricao) values (4, 'Câmera Canon 80D', 3500.0, sysdate(), 'O melhor ajuste de foco.');
 
-
 insert into cliente (id, nome, cpf) values (1, 'Fernando Medeiros', '000');
 insert into cliente (id, nome, cpf) values (2, 'Marcos Mariano', '111');
 
@@ -20,7 +19,10 @@ insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) value
 insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (3, 4, 3500, 1);
 insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (4, 1, 499, 1);
 
-insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (2, 'PROCESSANDO', 'cartao', '123', null);
+insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (1, 'RECEBIDO', 'cartao', '0123', null);
+insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (2, 'PROCESSANDO', 'cartao', '4567', null);
+insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (3, 'RECEBIDO', 'boleto', null, '8910');
+insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (4, 'PROCESSANDO', 'cartao', '1112', null);
 
 insert into nota_fiscal (pedido_id, data_emissao, xml) values (2, sysdate(), '<xml />');
 
