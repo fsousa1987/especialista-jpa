f3c4419e35cbbd1eb99593e947b24250e857cd05
fsousa1987 <franciscoeds1987@gmail.com>
2021-11-09 07:17:45 -0300

    9.35 - Criando subqueries

---
+++ src/test/java/com/francisco/ecommerce/jpql/SubqueriesTest.java
@@ -0,0 +1,38 @@
+package com.francisco.ecommerce.jpql;
+
+import com.francisco.ecommerce.EntityManagerTest;
+import com.francisco.ecommerce.model.Cliente;
+import java.util.List;
+import javax.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SubqueriesTest extends EntityManagerTest {
+
+  @SuppressWarnings("CommentedOutCode")
+  @Test
+  public void pesquisarSubqueries() {
+//         Bons clientes. Versão 2.
+    String jpql = "select c from Cliente c where " +
+        " 500 < (select sum(p.total) from Pedido p where p.cliente = c)";
+
+//         Bons clientes. Versão 1.
+//        String jpql = "select c from Cliente c where " +
+//                " 500 < (select sum(p.total) from c.pedidos p)";
+
+//         Todos os pedidos acima da média de vendas
+//        String jpql = "select p from Pedido p where " +
+//                " p.total > (select avg(total) from Pedido)";
+
+//         O produto ou os produtos mais caros da base.
+//        String jpql = "select p from Produto p where " +
+//                " p.preco = (select max(preco) from Produto)";
+
+    TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql, Cliente.class);
+
+    List<Cliente> lista = typedQuery.getResultList();
+    Assertions.assertFalse(lista.isEmpty());
+
+    lista.forEach(obj -> System.out.println("ID: " + obj.getId() + ", Nome: " + obj.getNome()));
+  }
+}
